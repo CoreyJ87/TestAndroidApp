fd34db3b5c6fd71b9bbfb7979bb6a1f1924a12cb
synik4l <synik4l@gmail.com>
2015-06-04 18:58:55 -0500

    Added immersion mode

--- app/src/main/java/info/coreyjones/witcher3map/Witcher3Map.java
+++ app/src/main/java/info/coreyjones/witcher3map/Witcher3Map.java
@@ -1,35 +1,46 @@
 package info.coreyjones.witcher3map;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class Witcher3Map extends ActionBarActivity {
+    public boolean fullScreen = false;
+    public final String siteURL = "http://witcher3map.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_witcher3_map);
-        WebView mapView = createMapView();
+        createMapView();
     }
 
-    protected WebView createMapView(){
+    protected WebView createMapView() {
         WebView mapView = (WebView) findViewById(R.id.mapView);
         mapView.setWebViewClient(new WebViewClient());
+
         WebSettings webSettings = mapView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setLoadsImagesAutomatically(true);
         webSettings.setSupportZoom(true);
-        mapView.loadUrl("http://witcher3map.com");
+
+        mapView.loadUrl(siteURL);
         return mapView;
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -47,10 +58,25 @@ public class Witcher3Map extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_immersive) {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            toggleHideyBar();
+            if (fullScreen) {
+                CharSequence text = "Full Screen mode: OFF!";
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                fullScreen = !fullScreen;
+            } else {
+                CharSequence text = "Full Screen mode: ON!";
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                fullScreen = !fullScreen;
+            }
         }
-
         return super.onOptionsItemSelected(item);
     }
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         WebView myWebView = (WebView) findViewById(R.id.mapView);
@@ -63,4 +89,31 @@ public class Witcher3Map extends ActionBarActivity {
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event);
     }
+
+    public void toggleHideyBar() {
+
+        int uiOptions = this.getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+
+        boolean isImmersiveModeEnabled =
+                ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+        // Immersive mode: Backward compatible to KitKat.
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+
+        this.getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+        //END_INCLUDE (set_ui_flags)
+    }
+
+
 }
