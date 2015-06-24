4a65250ba9c8e0689ad0d9c212bfb59e6e5386dd
synik4l <synik4l@gmail.com>
2015-06-23 23:31:58 -0500

    Added volley to project and a couple sensors working toward getting temp

--- app/src/main/java/info/coreyjones/TestApp/sensorsMenu.java
+++ app/src/main/java/info/coreyjones/TestApp/sensorsMenu.java
@@ -1,10 +1,13 @@
 package info.coreyjones.TestApp;
+import android.support.v7.app.ActionBarActivity;
+
 
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,31 +16,47 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-import java.util.Iterator;
 import java.util.List;
 
 
-public class sensorsMenu extends ActionBarActivity implements SensorEventListener {
-
+public class sensorsMenu extends ActionBarActivity implements SensorEventListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mCurrentLocation;
     private SensorManager mSensorManager;
-    private Sensor mLight;
+    private Sensor mLight, mMagnetic, mBarometer, mHumidity;
     SensorManager smm;
     List<Sensor> sensor;
     ListView lv;
+    protected boolean weatherOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensors_menu);
+        buildGoogleApiClient();
+
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         smm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        lv = (ListView) findViewById (R.id.listView1);
+
+
+        lv = (ListView) findViewById(R.id.listView);
         sensor = smm.getSensorList(Sensor.TYPE_ALL);
-        lv.setAdapter(new ArrayAdapter<Sensor>(this, android.R.layout.simple_list_item_1,  sensor));
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lv.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sensor));
+
+        mMagnetic = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mBarometer = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        mHumidity = mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
     }
 
 
@@ -48,19 +67,83 @@ public class sensorsMenu extends ActionBarActivity implements SensorEventListene
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        // The light sensor returns a single value.
-        // Many sensors return 3 values, one for each axis.
-        float lux = event.values[0];
-        String luxString = String.format("Light Sensor(Lux) Value: %s", String.valueOf(lux));
-        // Do something with this sensor value.
-        TextView lightSensor = (TextView) findViewById(R.id.lightSensorValue);
-        lightSensor.setText(luxString);
+        float x, y, z;
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            float lux = event.values[0];
+            String luxString = String.format("Light Sensor(Lux) Value: %s", String.valueOf(lux));
+            TextView lightSensor = (TextView) findViewById(R.id.lightSensorValue);
+            lightSensor.setText(luxString);
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            TextView magnetSensor = (TextView) findViewById(R.id.magnetvalues);
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
+            String fullString = String.format("Magnetometer: \nX: %s\nY: %s\nZ: %s", x, y, z);
+            magnetSensor.setText(fullString);
+        } else if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
+            TextView pressureSensor = (TextView) findViewById(R.id.barometerValue);
+            String pressureString = String.format("Barometric Pressure:\n%s:hPa(millibar)", event.values[0]);
+            pressureSensor.setText(pressureString);
+        } else if (event.sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY) {
+            TextView humiditySensor = (TextView) findViewById(R.id.humidityValue);
+            float rh = event.values[0];
+            humiditySensor.setText(String.format("Relative Humidity: %s", rh));
+            if (mCurrentLocation != null) {
+                double tempF = getTemp();
+            }
+        }
+    }
+
+    protected double getTemp() {
+        double temp = 0.0;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        TextView latitude = (TextView) findViewById(R.id.lat);
+        TextView longitudeVal = (TextView) findViewById(R.id.longitude);
+        double lat = mCurrentLocation.getLatitude();
+        double longitude = mCurrentLocation.getLongitude();
+
+        latitude.setText(String.valueOf(lat));
+        longitudeVal.setText(String.valueOf(longitude));
+
+        if(weatherOnce == false) {
+            String url = String.format("http://api.worldweatheronline.com/free/v2/weather.ashx?q=%s,%s", lat, longitude);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            TextView responseText = (TextView) findViewById(R.id.responseText);
+                            responseText.setText("Response is: " + response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    TextView responseText = (TextView) findViewById(R.id.responseText);
+                    responseText.setText("That didn't work!");
+                }
+            });
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+            weatherOnce = !weatherOnce;
+        }
+        return temp;
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        mSensorManager.registerListener(this, mBarometer, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mMagnetic, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -68,9 +151,6 @@ public class sensorsMenu extends ActionBarActivity implements SensorEventListene
         super.onPause();
         mSensorManager.unregisterListener(this);
     }
-
-
-
 
 
     @Override
@@ -94,4 +174,24 @@ public class sensorsMenu extends ActionBarActivity implements SensorEventListene
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        //TextView connectionStatus = (TextView) findViewById(R.id.connectStatus);
+        //connectionStatus.setText("Connected");
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //TextView connectStatus = (TextView) findViewById(R.id.connectStatus);
+        //connectStatus.setText("Suspended Connection");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //TextView connectStatus = (TextView) findViewById(R.id.connectStatus);
+        //connectStatus.setText("Connect to lat/long Failed");
+    }
+
 }
