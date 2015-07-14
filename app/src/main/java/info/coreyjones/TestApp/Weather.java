1e999f032acc75a14b2ada6cdcd3a609bb7fe044
synik4l <synik4l@gmail.com>
2015-07-13 23:33:08 -0500

    Refactored weather activity using butterknife

--- app/src/main/java/info/coreyjones/TestApp/Weather.java
+++ app/src/main/java/info/coreyjones/TestApp/Weather.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,7 +26,10 @@ import com.google.android.gms.location.LocationServices;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONStringer;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -34,13 +38,23 @@ import java.util.Date;
 public class Weather extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private GoogleApiClient mGoogleApiClient;
     private Location mCurrentLocation;
+    final String apiKey = "a851d887d5e2d63218ba800ec5bb4";
+    final String apiUrl = "http://api.worldweatheronline.com/free/v2/weather.ashx";
+    @Bind(R.id.responseText) TextView responseText;
+    @Bind(R.id.parsedData) TextView parsedTextView;
+    @Bind(R.id.longitude) TextView longitudeVal;
+    @Bind(R.id.lat) TextView latitude;
+    @Bind(R.id.lastUpdated) TextView lastUpdated;
+    @Bind(R.id.connectionStatus) TextView connectionStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        ButterKnife.bind(this);
         buildGoogleApiClient();
         restoreSensorText();
+
     }
 
     @Override
@@ -48,7 +62,6 @@ public class Weather extends ActionBarActivity implements GoogleApiClient.Connec
         getMenuInflater().inflate(R.menu.menu_weather, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -59,23 +72,27 @@ public class Weather extends ActionBarActivity implements GoogleApiClient.Connec
         return super.onOptionsItemSelected(item);
     }
 
+    //My Methods
     public void getWeatherButton(View view) {
         if (mCurrentLocation != null) {
-            TextView lastUpdated = (TextView) findViewById(R.id.lastUpdated);
+            //TextView lastUpdated = (TextView) findViewById(R.id.lastUpdated);
             String updateTime = DateFormat.getTimeInstance().format(new Date());
             lastUpdated.setText(updateTime);
-            getWeather(getCurrentFocus());
+            getWeather();
         }
     }
+    @OnClick(R.id.refreshWeather) void refreshWeatherButton(Button button) {
+        String updateTime = DateFormat.getTimeInstance().format(new Date());
+        lastUpdated.setText(updateTime);
+        getWeather();
+    }
 
-    //My Methods
-    private void getWeather(final View view) {
+    private void getWeather() {
         RequestQueue queue = Volley.newRequestQueue(this);
-        final String apiKey = "a851d887d5e2d63218ba800ec5bb4";
-        final String apiUrl = "http://api.worldweatheronline.com/free/v2/weather.ashx";
         double lat = getLat();
         double longitude = getLong();
         String url = String.format("%s?q=%s,%s&key=%s&format=JSON", apiUrl, lat, longitude, apiKey);
+
 
         updateLatLng(lat, longitude);
 
@@ -83,14 +100,12 @@ public class Weather extends ActionBarActivity implements GoogleApiClient.Connec
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        TextView responseText = (TextView) findViewById(R.id.responseText);
                         responseText.setText("Response is: " + response);
                         parseJson(response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                TextView responseText = (TextView) findViewById(R.id.responseText);
                 responseText.setText("That didn't work!");
             }
         });
@@ -104,7 +119,7 @@ public class Weather extends ActionBarActivity implements GoogleApiClient.Connec
                 String weatherOutput = "Current Conditions:";
                 JSONObject reader = new JSONObject(theResponse);
                 JSONObject data = reader.getJSONObject("data");
-                TextView parsedTextView = (TextView) findViewById(R.id.parsedData);
+
                 parsedTextView.setText(weatherOutput);
                 //Top levels of the json
                 JSONArray current_condition_array = data.optJSONArray("current_condition");
@@ -112,20 +127,18 @@ public class Weather extends ActionBarActivity implements GoogleApiClient.Connec
 
                 //Current Conditions. We dont loop the obj cus theres only one current condition
                 JSONObject current_condition_obj = current_condition_array.getJSONObject(0);
-                for(int i = 0; i<current_condition_obj.names().length(); i++){
+                for (int i = 0; i < current_condition_obj.names().length(); i++) {
                     Log.v("Response", "key = " + current_condition_obj.names().getString(i) + " value = " + current_condition_obj.get(current_condition_obj.names().getString(i)));
-                    if(current_condition_obj.names().getString(i) == "weatherDesc") {
+                    if (current_condition_obj.names().getString(i) == "weatherDesc") {
                         JSONArray weatherDescArr = current_condition_obj.optJSONArray("weatherDesc");
                         JSONObject weatherDescobj = weatherDescArr.getJSONObject(0);
-                        parsedTextView.append(String.format("\nWeather Description: %s",weatherDescobj.getString("value")));
+                        parsedTextView.append(String.format("\nWeather Description: %s", weatherDescobj.getString("value")));
                     }
                     parsedTextView.append(String.format("\n%s: %s", current_condition_obj.names().getString(i), current_condition_obj.get(current_condition_obj.names().getString(i))));
                 }
 
                 //Output to textView
-
-
-                saveSensorText(getCurrentFocus());
+                saveSensorText();
                 /*
                 //Weather by date. Loop later because it shows up to 5 days
                 for(int x=0;x< weather_array.length();x++){
@@ -184,17 +197,12 @@ public class Weather extends ActionBarActivity implements GoogleApiClient.Connec
     }
 
     private void updateLatLng(double lat, double longitude) {
-        TextView latitude = (TextView) findViewById(R.id.lat);
-        TextView longitudeVal = (TextView) findViewById(R.id.longitude);
         latitude.setText(String.valueOf(lat));
         longitudeVal.setText(String.valueOf(longitude));
     }
 
-    private void saveSensorText(View view) {
-        //Grab text from editText field
-        TextView unparsedTextView = (TextView) findViewById(R.id.responseText);
-        TextView parsedTextView = (TextView) findViewById(R.id.parsedData);
-        String unparsed = unparsedTextView.getText().toString();
+    private void saveSensorText() {
+        String unparsed = responseText.getText().toString();
         String parsed = parsedTextView.getText().toString();
 
         //Save text to preferences
@@ -215,19 +223,13 @@ public class Weather extends ActionBarActivity implements GoogleApiClient.Connec
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
         String theRestoredresponse = sharedPref.getString("unparsed_json", defaultResponseVal);
         String theRestoredTime = sharedPref.getString("the_time", defaultTimeVal);
-        String theRestoredParsed = sharedPref.getString("parsed_json",defaultParsedJson);
+        String theRestoredParsed = sharedPref.getString("parsed_json", defaultParsedJson);
 
         if (!theRestoredresponse.equals(defaultResponseVal)) {
             toastMessage(getString(R.string.textRestored));
         }
-
-        TextView responseText = (TextView) findViewById(R.id.responseText);
         responseText.setText(theRestoredresponse);
-
-        TextView parsedTextView = (TextView) findViewById(R.id.parsedData);
         parsedTextView.setText(theRestoredParsed);
-
-        TextView lastUpdated = (TextView) findViewById(R.id.lastUpdated);
         lastUpdated.setText(theRestoredTime);
     }
 
@@ -243,22 +245,19 @@ public class Weather extends ActionBarActivity implements GoogleApiClient.Connec
 
     @Override
     public void onConnected(Bundle bundle) {
-        //TextView connectionStatus = (TextView) findViewById(R.id.connectStatus);
-        //connectionStatus.setText("Connected");
+        connectionStatus.setText("Connected");
         mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         updateLatLng(getLat(), getLong());
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        //TextView connectStatus = (TextView) findViewById(R.id.connectStatus);
-        //connectStatus.setText("Suspended Connection");
+        connectionStatus.setText("Suspended Connection");
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        //TextView connectStatus = (TextView) findViewById(R.id.connectStatus);
-        //connectStatus.setText("Connect to lat/long Failed");
+        connectionStatus.setText("Connect to lat/long Failed");
     }
 
     private void toastMessage(CharSequence text) {
